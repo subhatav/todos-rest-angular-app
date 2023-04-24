e52fb7d579f57f81250b6471a0debd17b977f85e
Subhatav Dhali <subhatav@gmail.com>
2023-04-24 21:49:50 +0530

    Create a Spring Boot project with required properties

---
+++ todos-backend/src/main/java/com/ph4ntom/of/codes/todosbackend/TodosApplication.java
@@ -0,0 +1,13 @@
+package com.ph4ntom.of.codes.todosbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodosApplication {
+
+  public static void main(final String[] arguments) {
+
+    SpringApplication.run(TodosApplication.class, arguments);
+  }
+}
---
+++ todos-backend/src/test/java/com/ph4ntom/of/codes/todosbackend/TodosApplicationTests.java
@@ -0,0 +1,11 @@
+package com.ph4ntom.of.codes.todosbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
